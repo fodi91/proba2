6457a9deeb8e34390f4e3dfd40e2b24fcfd0ad5f
fodi91 <fodi91@gmail.com>
2022-01-04 19:29:05 +0100

    'ok'

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const btnContainerEl = document.querySelector(".button-container");
 console.log(btnContainerEl);
 const fetchText = function (e, textId) {
   const reguest = new Request(`${e.target.textContent}.txt`);
-  console.log(reguest).then((response) => response.text());
-  fetch(request);
+  fetch(reguest)
+    .then((response) => response.text())
+    .then((text) => navigator.clipboard.writeText(text));
 };
 
 btnContainerEl.addEventListener("click", fetchText);
